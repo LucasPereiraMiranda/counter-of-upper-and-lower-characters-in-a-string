ea921384cc1d8bef577cdc30c2fd78c1929b76b5
Lucas Pereira Miranda <39305813+LucasPereiraMiranda@users.noreply.github.com>
2019-02-02 10:21:13 -0200

    developed in python

---
+++ counter__of__upper__and__lower__characters__in__a__string.py
@@ -0,0 +1,20 @@
+
+def counter_lower_upper_case(string):
+    lower_case = 0
+    upper_case = 0
+    for caracter in string:
+        if('A'<=caracter<='Z'):
+            upper_case +=1
+        elif('a'<=caracter<='z'):
+            lower_case+=1
+        else:
+            continue
+    return lower_case,upper_case
+
+
+string = input("Enter a string: ")
+lower_case,upper_case = counter_lower_upper_case(string)
+print("The string {0} has {1} upper and lower"
+      " {2} characters".format(string,str(upper_case),str(lower_case)))
+
+
